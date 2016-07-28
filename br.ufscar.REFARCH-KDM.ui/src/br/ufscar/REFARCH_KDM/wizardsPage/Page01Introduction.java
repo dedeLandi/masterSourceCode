66d2b0fce54c620ec03036d59bdba272ad62403b
André <a_souzalandy@yahoo.com.br>
2016-07-28 18:23:44 -0300

    Commit 1 - Wizard page 01 of the plug-in REFARCH-KDM

---
+++ br.ufscar.REFARCH-KDM.ui/src/br/ufscar/REFARCH_KDM/wizardsPage/Page01Introduction.java
@@ -0,0 +1,41 @@
+package br.ufscar.REFARCH_KDM.wizardsPage;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class Page01Introduction extends WizardPage {
+
+	/**
+	 * Create the wizard.
+	 */
+	public Page01Introduction() {
+		super("wizardPage");
+		setPageComplete(false);
+		setMessage("");
+		
+		setTitle("Architectural Refactoring Wizard");
+		setDescription("");
+	}
+
+	/**
+	 * Create contents of the wizard.
+	 * @param parent
+	 */
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+
+		setControl(container);
+		
+		Label lDescritption = new Label(container, SWT.WRAP | SWT.HORIZONTAL);
+		lDescritption.setBounds(10, 10, 554, 314);
+		lDescritption.setText("Hello developer!\r\n\r\nThis wizard aims to facilitate your architectural refactoring based on architectural drifts previously discovered.\r\n\r\nLets go to start the wizard!");
+	}
+	
+	@Override
+	public boolean canFlipToNextPage() {
+		return true;
+	}
+	
+}
