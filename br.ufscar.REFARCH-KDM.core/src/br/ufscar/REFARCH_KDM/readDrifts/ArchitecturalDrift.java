c0765c2de121eb485a03fea459acf858fff73d8e
André <a_souzalandy@yahoo.com.br>
2016-08-02 17:24:28 -0300

    Atividade 21 - Desvio Arquitetural

--- br.ufscar.REFARCH-KDM.core/src/br/ufscar/REFARCH_KDM/readDrifts/ArchitecturalDrift.java
+++ br.ufscar.REFARCH-KDM.core/src/br/ufscar/REFARCH_KDM/readDrifts/ArchitecturalDrift.java
@@ -3,6 +3,29 @@ package br.ufscar.REFARCH_KDM.readDrifts;
 public class ArchitecturalDrift {
 
 	private String description = "";
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
+	
 	
 	
 }
