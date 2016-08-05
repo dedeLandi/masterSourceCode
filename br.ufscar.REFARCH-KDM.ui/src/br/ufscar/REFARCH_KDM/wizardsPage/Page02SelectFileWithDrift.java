091ed58faae1d99c5b319862bb04b60d20bdb755
André <a_souzalandy@yahoo.com.br>
2016-08-05 15:53:15 -0300

    Atividade 2 - Wizard page 02 of the plug-in REFARCH-KDM

--- br.ufscar.REFARCH-KDM.ui/src/br/ufscar/REFARCH_KDM/wizardsPage/Page02SelectFileWithDrift.java
+++ br.ufscar.REFARCH-KDM.ui/src/br/ufscar/REFARCH_KDM/wizardsPage/Page02SelectFileWithDrift.java
@@ -1,7 +1,9 @@
 package br.ufscar.REFARCH_KDM.wizardsPage;
 
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.layout.GridData;
@@ -13,6 +15,7 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import br.ufscar.REFARCH_KDM.readDrifts.ReadDriftsAlgorithm;
@@ -21,6 +24,8 @@ public class Page02SelectFileWithDrift extends WizardPage {
 
 	private Combo cbAlgorithm;
 	private Text tPathFileDrifts;
+	private Text tPathFilePlanned;
+	private Text tPathFileActual;
 
 	/**
 	 * Create the wizard.
@@ -28,7 +33,7 @@ public class Page02SelectFileWithDrift extends WizardPage {
 	public Page02SelectFileWithDrift() {
 		super("page02");
 		setTitle("Architectural Refactoring Wizard");
-		setDescription("Select the file containing the architectural drifts.");
+		setDescription("Select the algorithm and the files containing the architectural drifts, planned architecture and actual architecture.");
 	}
 
 	/**
@@ -60,27 +65,61 @@ public class Page02SelectFileWithDrift extends WizardPage {
 
 		});
 
-		Label lFile = new Label(container, SWT.NONE);
-		lFile.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		lFile.setText("File containing the drifts");
+		Label lFileDrifts = new Label(container, SWT.NONE);
+		lFileDrifts.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lFileDrifts.setText("File containing the drifts");
 
 		tPathFileDrifts = new Text(container, SWT.BORDER);
 		tPathFileDrifts.setEditable(false);
 		tPathFileDrifts.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
-		Button bSearch = new Button(container, SWT.NONE);
-		bSearch.addMouseListener(new MouseAdapter() {
+		Button bSearchDrifts = new Button(container, SWT.NONE);
+		bSearchDrifts.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				chooseFile();
+				chooseFileDrifts();
 			}
 		});
-		bSearch.setText("Search");
+		bSearchDrifts.setText("Search");
+
+		Label lFilePlannedArchitecture = new Label(container, SWT.NONE);
+		lFilePlannedArchitecture.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lFilePlannedArchitecture.setText("File containing the \r\nplanned architecture");
+
+		tPathFilePlanned = new Text(container, SWT.BORDER);
+		tPathFilePlanned.setEditable(false);
+		tPathFilePlanned.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		Button bSearchPlanned = new Button(container, SWT.NONE);
+		bSearchPlanned.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				chooseFilePlanned();
+			}
+		});
+		bSearchPlanned.setText("Search");
+
+		Label lFileActualArchitecture = new Label(container, SWT.NONE);
+		lFileActualArchitecture.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lFileActualArchitecture.setText("File containing the \r\nactual architecture");
+
+		tPathFileActual = new Text(container, SWT.BORDER);
+		tPathFileActual.setEditable(false);
+		tPathFileActual.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		Button bSearchActual = new Button(container, SWT.NONE);
+		bSearchActual.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				chooseFileActual();
+			}
+		});
+		bSearchActual.setText("Search");
 
 		fillCbAlgorithm();
 	}
 
-	protected void chooseFile() {
+	protected void chooseFileDrifts() {
 
 		if(cbAlgorithm.getSelectionIndex() == -1){
 			setErrorMessage("Select one type of algorithm to continue.");
@@ -90,15 +129,53 @@ public class Page02SelectFileWithDrift extends WizardPage {
 			FileDialog fd = new FileDialog(getShell(), SWT.OPEN);
 			fd.setText("Open");
 			fd.setFilterPath("C:/");
-			String[] filterExt = ReadDriftsAlgorithm.valueOf(cbAlgorithm.getText()).getExtensions();
+			String[] filterExt = ReadDriftsAlgorithm.getEnumObject(cbAlgorithm.getText()).getExtensions();
 			fd.setFilterExtensions(filterExt);
 			String selected = fd.open();
 			if(selected == null){
 				tPathFileDrifts.setText("");
 			}else{
-				System.out.println("Path of the XMI file:" + selected);
+				System.out.println("Path of the XMI drifted file:" + selected);
 				tPathFileDrifts.setText(selected);
 			}
+		}
+		getWizard().getContainer().updateButtons();
+	}
+
+	protected void chooseFilePlanned() {
+
+		setErrorMessage(null); // clear error message. 
+
+		FileDialog fd = new FileDialog(getShell(), SWT.OPEN);
+		fd.setText("Open");
+		fd.setFilterPath("C:/");
+		String[] filterExt = new String[]{"*.xmi"};
+		fd.setFilterExtensions(filterExt);
+		String selected = fd.open();
+		if(selected == null){
+			tPathFilePlanned.setText("");
+		}else{
+			System.out.println("Path of the XMI planned file:" + selected);
+			tPathFilePlanned.setText(selected);
+		}
+		getWizard().getContainer().updateButtons();
+	}
+
+	protected void chooseFileActual() {
+
+		setErrorMessage(null); // clear error message. 
+
+		FileDialog fd = new FileDialog(getShell(), SWT.OPEN);
+		fd.setText("Open");
+		fd.setFilterPath("C:/");
+		String[] filterExt = new String[]{"*.xmi"};
+		fd.setFilterExtensions(filterExt);
+		String selected = fd.open();
+		if(selected == null){
+			tPathFileActual.setText("");
+		}else{
+			System.out.println("Path of the XMI actual file:" + selected);
+			tPathFileActual.setText(selected);
 		}
 		getWizard().getContainer().updateButtons();
 	}
@@ -123,7 +200,25 @@ public class Page02SelectFileWithDrift extends WizardPage {
 
 	private boolean validateTPathFileDrifts() {
 		if("".equalsIgnoreCase(tPathFileDrifts.getText())) { 
-			setErrorMessage("Select one file to continue.");
+			setErrorMessage("Select one file to continue. (Drifts)");
+			return false;
+		}else{
+			setErrorMessage(null); // clear error message. 
+			return true;
+		}
+	}
+	private boolean validateTPathFilePlanned() {
+		if("".equalsIgnoreCase(tPathFilePlanned.getText())) { 
+			setErrorMessage("Select one file to continue. (Planned)");
+			return false;
+		}else{
+			setErrorMessage(null); // clear error message. 
+			return true;
+		}
+	}
+	private boolean validateTPathFileActual() {
+		if("".equalsIgnoreCase(tPathFileActual.getText())) { 
+			setErrorMessage("Select one file to continue. (Actual)");
 			return false;
 		}else{
 			setErrorMessage(null); // clear error message. 
@@ -132,16 +227,37 @@ public class Page02SelectFileWithDrift extends WizardPage {
 	}
 
 	@Override
+	public void performHelp(){
+		//TODO Desenvolver o help dessa pagina
+	    Shell shell = new Shell(getShell());
+	    shell.setText("My Custom Help !!");
+	    shell.setLayout(new GridLayout());
+	    shell.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+	    Browser browser = new Browser(shell, SWT.NONE);
+	    browser.setUrl("http://stackoverflow.com/questions/7322489/cant-put-content-behind-swt-wizard-help-button");
+	    browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+	    shell.open();
+	}
+	
+	@Override
 	public boolean canFlipToNextPage() {
-		return validateCbAlgorithm() && validateTPathFileDrifts();
+		return validateCbAlgorithm() && validateTPathFileDrifts() && validateTPathFilePlanned() && validateTPathFileActual();
 	}
 
 	public String getPathKDMFile() {
 		return tPathFileDrifts.getText();
 	}
-	
+
 	public ReadDriftsAlgorithm getAlgorithmType(){
-		return ReadDriftsAlgorithm.valueOf(cbAlgorithm.getText());
+		return ReadDriftsAlgorithm.getEnumObject(cbAlgorithm.getText());
 	}
-	
+
+	@Override
+	public IWizardPage getNextPage() {
+		((Page03SelectDrift) super.getNextPage()).fillTDrifts();
+		return super.getNextPage();
+	}
+
 }
