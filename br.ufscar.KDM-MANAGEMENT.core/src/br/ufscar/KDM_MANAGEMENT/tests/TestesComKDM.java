89cbc62e923ad8aee8386f26a355800dbf9cae61
André <a_souzalandy@yahoo.com.br>
2016-08-05 18:25:42 -0300

    Atividade 14 - Testes

--- br.ufscar.KDM-MANAGEMENT.core/src/br/ufscar/KDM_MANAGEMENT/tests/TestesComKDM.java
+++ br.ufscar.KDM-MANAGEMENT.core/src/br/ufscar/KDM_MANAGEMENT/tests/TestesComKDM.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.gmt.modisco.omg.kdm.code.Package;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.KDMModel;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
 
 import br.ufscar.KDM_MANAGEMENT.load.KDMFileReader;
 import br.ufscar.KDM_MANAGEMENT.models.KDMModelReader;
+import br.ufscar.KDM_MANAGEMENT.packages.KDMPackageReader;
 
 public class TestesComKDM {
 
@@ -16,6 +18,15 @@ public class TestesComKDM {
 		String KDMpathDrifts = "C:\\TestsPlug-in\\labSystemKDMComdesvios.xmi";
 		
 		Object KDMFile = new KDMFileReader(KDMpathDrifts, KDMFileReader.READ_KDM_TO_SEGMENT).getKdmRead();
+		
+		System.out.println("-----------------------------Models--------------------------------");
+		
+		getKDMModels(KDMFile);
+		
+		System.out.println("-----------------------------Packages--------------------------------");
+
+		getKDMPackages(KDMFile);
+		
 		
 		List<ArchitecturalDrift> drifts = getKDMDriftsRead(KDMFile);
 		
@@ -25,9 +36,28 @@ public class TestesComKDM {
 
 	}
 
-	private static List<ArchitecturalDrift> getKDMDriftsRead(Object kDMFile) {
+	private static void getKDMPackages(Object kDMFile) {
+		Segment KDMTree = (Segment) kDMFile;
 		
-		List<ArchitecturalDrift> driftsRead = new ArrayList<>();
+		Map<String, List<Package>> packagesPerCodeModel = new KDMPackageReader(KDMTree).getAllPackages();
+		
+		for (String nameCodeModel : packagesPerCodeModel.keySet()) {
+			
+			List<Package> packages = packagesPerCodeModel.get(nameCodeModel);
+			
+			String packagesString = "";
+			
+			for (Package pack : packages) {
+				packagesString = packagesString.concat(pack.getName() + ";");
+			}
+			
+			System.out.println("Code Model:" + nameCodeModel + " - Packages: " + packagesString);
+		}
+		
+		
+	}
+
+	private static void getKDMModels(Object kDMFile) {
 		Segment KDMTree = (Segment) kDMFile;
 		Map<String, Map<String, List<KDMModel>>> allModels = new KDMModelReader(KDMTree).getAllModels();
 		for (String key : allModels.keySet()) {
@@ -47,6 +77,12 @@ public class TestesComKDM {
 				System.out.println(key + " - null");
 			}
 		}
+	}
+
+	private static List<ArchitecturalDrift> getKDMDriftsRead(Object kDMFile) {
+		
+		List<ArchitecturalDrift> driftsRead = new ArrayList<>();
+		
 		
 		return driftsRead;
 	}
