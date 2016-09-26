8bc06af2b4e8297d4f66b270c288168cdafb2a69
André <a_souzalandy@yahoo.com.br>
2016-09-26 20:01:17 -0300

    Atividade 65 - Recover de Relações do tipo HasValue

---
+++ br.ufscar.KDM-MANAGEMENT.core/src/br/ufscar/KDM_MANAGEMENT/recovers/recoverRelations/impl/recovers/codeRelations/RecoverRelationsHasValueImpl.java
@@ -0,0 +1,215 @@
+package br.ufscar.KDM_MANAGEMENT.recovers.recoverRelations.impl.recovers.codeRelations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.gmt.modisco.omg.kdm.action.ActionElement;
+import org.eclipse.gmt.modisco.omg.kdm.action.BlockUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.AbstractCodeElement;
+import org.eclipse.gmt.modisco.omg.kdm.code.AbstractCodeRelationship;
+import org.eclipse.gmt.modisco.omg.kdm.code.ClassUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeItem;
+import org.eclipse.gmt.modisco.omg.kdm.code.HasValue;
+import org.eclipse.gmt.modisco.omg.kdm.code.InterfaceUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.MethodUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Package;
+import org.eclipse.gmt.modisco.omg.kdm.code.ParameterUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Signature;
+import org.eclipse.gmt.modisco.omg.kdm.code.StorableUnit;
+import org.eclipse.gmt.modisco.omg.kdm.core.KDMRelationship;
+
+import br.ufscar.KDM_MANAGEMENT.recovers.recoverRelations.interfaces.RecoverRelations;
+
+public class RecoverRelationsHasValueImpl implements RecoverRelations {
+
+	@Override
+	public List<KDMRelationship> getRelationOf(ClassUnit classToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (CodeItem codeItem : classToAvaliate.getCodeElement()) {
+
+			if(codeItem instanceof StorableUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((StorableUnit) codeItem));
+
+			}else if(codeItem instanceof MethodUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((MethodUnit) codeItem));
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(Package packageToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (AbstractCodeElement abstractCodeElement : packageToAvaliate.getCodeElement()) {
+
+			if(abstractCodeElement instanceof ClassUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((ClassUnit) abstractCodeElement));
+			}else if(abstractCodeElement instanceof InterfaceUnit){
+				hasValueRelations.addAll(this.getRelationOf((InterfaceUnit) abstractCodeElement));
+			}else if(abstractCodeElement instanceof Package){
+
+				hasValueRelations.addAll(this.getRelationOf((Package) abstractCodeElement));
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(BlockUnit blockToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (AbstractCodeElement abstractCodeElement : blockToAvaliate.getCodeElement()) {
+
+			if(abstractCodeElement instanceof StorableUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((StorableUnit) abstractCodeElement));
+				
+			}else if(abstractCodeElement instanceof ActionElement){
+
+				hasValueRelations.addAll(this.getRelationOf((ActionElement) abstractCodeElement));
+
+			}else if(abstractCodeElement instanceof BlockUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((BlockUnit) abstractCodeElement));
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(ActionElement actionElementToAvaliate) {
+		
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (AbstractCodeElement abstractCodeElement : actionElementToAvaliate.getCodeElement()) {
+
+			if(abstractCodeElement instanceof StorableUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((StorableUnit) abstractCodeElement));
+				
+			}else if(abstractCodeElement instanceof ActionElement){
+
+				hasValueRelations.addAll(this.getRelationOf((ActionElement) abstractCodeElement));
+
+			}else if(abstractCodeElement instanceof BlockUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((BlockUnit) abstractCodeElement));
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(Signature signatureToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (ParameterUnit parameterUnit : signatureToAvaliate.getParameterUnit()) {
+			hasValueRelations.addAll(this.getRelationOf(parameterUnit));
+		}
+
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(ParameterUnit parameterToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (AbstractCodeRelationship abstractCodeRelationship : parameterToAvaliate.getCodeRelation()) {
+
+			if(abstractCodeRelationship instanceof HasValue){
+
+				hasValueRelations.add((HasValue) abstractCodeRelationship);
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(StorableUnit storableToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (AbstractCodeRelationship abstractCodeRelationship : storableToAvaliate.getCodeRelation()) {
+
+			if(abstractCodeRelationship instanceof HasValue){
+
+				hasValueRelations.add((HasValue) abstractCodeRelationship);
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(MethodUnit methodToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (AbstractCodeElement abstractCodeElement : methodToAvaliate.getCodeElement()) {
+
+			if(abstractCodeElement instanceof Signature){
+
+				hasValueRelations.addAll(this.getRelationOf((Signature) abstractCodeElement));
+
+			}else if(abstractCodeElement instanceof BlockUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((BlockUnit) abstractCodeElement));
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+
+	@Override
+	public List<KDMRelationship> getRelationOf(InterfaceUnit interfaceToAvaliate) {
+
+		List<KDMRelationship> hasValueRelations = new ArrayList<KDMRelationship>();
+
+		for (CodeItem codeItem : interfaceToAvaliate.getCodeElement()) {
+
+			if(codeItem instanceof StorableUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((StorableUnit) codeItem));
+
+			}else if(codeItem instanceof MethodUnit){
+
+				hasValueRelations.addAll(this.getRelationOf((MethodUnit) codeItem));
+
+			}
+
+		}
+
+		return hasValueRelations;
+	}
+	
+}
